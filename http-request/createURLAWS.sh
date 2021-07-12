739ac846e3b5bbd9b43083b0f95b47bb820c6f86
Enrique M. Talavera <kiquetal@gmail.com>
2021-07-11 21:02:29 -0400

    adding signedurl for put

---
+++ http-request/createURLAWS.sh
@@ -0,0 +1,1 @@
+http POST https://jr4nshn6vb.execute-api.us-east-1.amazonaws.com/dev/upload-url/user-5  x-api-key:6cgYx6cEn09oDLlHHHIZn4uXmx4OLEAH7uv4BraL	< createURL.json
--- upload/main.js
+++ upload/main.js
@@ -4,19 +4,38 @@ const dynamodb = require('serverless-dynamodb-client');
 const Joi = require('joi');
 const httpStatus = require('http-status');
 const uuid = require('uuid');
-const util = require('./util');
+const util = require('../templates/util');
+const AWS = require('aws-sdk');
+const S3= new AWS.S3();
 module.exports.handler = async (event, context) => {
     const schema = Joi.object({
-        user_id: Joi.string()
-            .required(),
-        message_text: Joi.string()
-            .required(),
-        template_name: Joi.string()
-            .required(),
-        idempotent_key:Joi.string()
-            .optional()
+        file_name: Joi.string()
+            .required()
     });
     try {
+
+        const {value, error, warning} = schema.validate(JSON.parse(event.body),{allowUnknown:true});
+        if (error)
+            util.returnError(400,error.stack);
+
+        const bucketName = process.env['BUCKET_NAME'];
+        console.log(bucketName);
+        const bucketKey = value['file_name'];
+        const contentType = value['content_type'];
+        console.log(contentType);
+        const params = {
+            Bucket:bucketName,
+            Key:bucketKey,
+            ContentType:contentType
+        };
+        console.log(JSON.stringify(params));
+        var url =  S3.getSignedUrl('putObject', params);
+
+        return {
+            statusCode: httpStatus.OK,
+            body:JSON.stringify({"message":url})
+
+        }
 
     }
     catch (e) {
