28ef81635617c43ef633450348be06d526dae6fb
Enrique M. Talavera <kiquetal@gmail.com>
2021-07-24 00:20:00 -0400

    init sqs logic

---
+++ http-request/createNotification.sh
@@ -0,0 +1,1 @@
+http POST http://localhost:4000/dev/notifications
--- notification/main.js
+++ notification/main.js
@@ -6,6 +6,7 @@ const uuid = require('uuid');
 const util = require('../templates/util');
 const AWS = require('aws-sdk');
 const S3= new AWS.S3();
+const SQS = new AWS.SQS();
 module.exports.handler = async (event, context) => {
     const schema = Joi.object({
         file_name: Joi.string()
@@ -13,6 +14,10 @@ module.exports.handler = async (event, context) => {
     });
     try {
 
+        const queueURL = process.env.QUEUE_URL;
+        const queueName = process.env.QUEUE_NAME;
+        console.log(queueURL);
+        console.log(queueName);
         const {value, error, warning} = schema.validate(JSON.parse(event.body),{allowUnknown:true});
         if (error)
             util.returnError(400,error.stack);
