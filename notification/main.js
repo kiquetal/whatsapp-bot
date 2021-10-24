3f2d001f35663008201326584e06e928e93fadb1
kiquetal <kiquetal@gmail.com>
2021-10-24 20:38:50 -0300

    Working s3 local

--- notification/main.js
+++ notification/main.js
@@ -5,9 +5,17 @@ const httpStatus = require('http-status');
 const uuid = require('uuid');
 const util = require('../templates/util');
 const AWS = require('aws-sdk');
-const S3= new AWS.S3();
 const SQS = new AWS.SQS();
 module.exports.handler = async (event, context) => {
+
+    const S3 = new AWS.S3({
+        s3ForcePathStyle: true,
+        accessKeyId: 'S3RVER', // This specific key is required when working offline
+        secretAccessKey: 'S3RVER',
+        endpoint: new AWS.Endpoint('http://localhost:4569'),
+    });
+
+
     const schema = Joi.object({
         message:Joi.string().required(),
         user_id:Joi.string().required(),
@@ -19,9 +27,18 @@ module.exports.handler = async (event, context) => {
 
         const queueURL = process.env.QUEUE_URL;
         const queueName = process.env.QUEUE_NAME;
-        const BUCKET_NAME = process.env.BUCKET_NAME;
+        const BUCKET_NAME = process.env.BUCKET_NAME
+        console.log(BUCKET_NAME);
         console.log(queueURL);
         console.log(queueName);
+
+        S3.putObject({
+            Bucket: BUCKET_NAME,
+            Key: '1234',
+            Body: new Buffer('abcd')
+        }, () => {} );
+
+
         const {value, error, warning} = schema.validate(JSON.parse(event.body),{allowUnknown:true});
         if (error)
           return  util.returnError(400,error.stack);
@@ -36,7 +53,7 @@ module.exports.handler = async (event, context) => {
 
             };
             try{
-                
+
 
            const result = await S3.getObject(params).promise()
             }
